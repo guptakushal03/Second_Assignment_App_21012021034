2c7c0f8c257b4ec2a21aa9c94d617164b5efbc55
Kushal Gupta <guptakushal2003@gmail.com>
2023-10-13 17:53:15 +0530

    Main Page Commit 1

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Second_Assignment_App_21012021034"
+include(":app")
+ 
